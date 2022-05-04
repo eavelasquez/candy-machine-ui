6d6d8153735134967db7ac34b3bd598eb8d96eb2
Esteban Velasquez <eavc.30@gmail.com>
2022-05-04 06:51:36 -0500

    feat(front): clean candy-machine ui project

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    // generate manifest.json in output directory
+    // https://vitejs.dev/config/#manifest
+    manifest: true,
+  },
   plugins: [react()]
 })
